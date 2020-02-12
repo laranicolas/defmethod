53db78797fad7e70e7daa5c65b19e7aa688df665
Nicolás Lara <laranicolas@gmail.com>
2020-02-12 18:15:43 -0300

    Display output results.

---
+++ lib/parser/display.rb
@@ -0,0 +1,37 @@
+require_relative 'sort'
+
+module Parser
+  module Display
+    extend self
+
+    def call(content)
+      output(content)
+    end
+
+    private
+
+      def output(content)
+        return 'EMPTY CONTENT' if content.empty?
+
+        one_sorted = Parser::Sort.by_gender_asc_then_last_name_asc(content)
+        two_sorted = Parser::Sort.by_dob_asc_then_last_name_asc(content)
+        third_sorted = Parser::Sort.by_last_name_desc(content)
+        <<~OUTPUT
+          Output 1:
+          #{output_line(one_sorted).join("\n")}
+
+          Output 2:
+          #{output_line(two_sorted).join("\n")}
+
+          Output 3:
+          #{output_line(third_sorted).join("\n")}
+        OUTPUT
+      end
+
+      def output_line(columns)
+        columns.map do |col|
+          "#{col[:last_name]} #{col[:first_name]} #{col[:gender]} #{col[:date_of_birth]} #{col[:favorite_color]}"
+        end
+      end
+  end
+end
